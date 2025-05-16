1220673e29eb031e03df8744cdbc7739250d5eb5
liamma06 <liam.jbr.ma@gmail.com>
2025-05-16 17:42:22 -0400

    homepage spotlight

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
-
-export default function Home() {
+"use client";
+import React from "react";
+import { Spotlight } from "@/components/ui/spotlight-new";
+ 
+export default function SpotlightNewDemo() {
   return (
-    <h1>HI</h1>
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.70] antialiased relative overflow-hidden">
+      <Spotlight />
+      <h1>WE Wrapped: A smarter look at your school year.</h1>
+    </div>
   );
 }
